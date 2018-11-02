59becfc4d614d5a71ae75c3a998cc6a47e2b5a99
aris@bedbugstudio.com <aris@bedbugstudio.com>
2018-11-02 19:54:55 +0200

    Added new trn_leaderboard_def model to substitute old pool inside the leaderpay module

---
+++ sportimo_modules/leaderpay/models/trn_leaderboard_def.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.Types.ObjectId;
+
+if (mongoose.models.trn_leaderboard_defs)
+    module.exports = mongoose.models.trn_leaderboard_defs;
+else {
+
+    var prizeDef = {
+        rank: { type: Number, min: 0 },
+        rank_from: { type: Number, min: 0 },
+        rank_to: { type: Number, min: 0 },
+        prize: { type: Schema.Types.ObjectId, ref: 'prizes' }
+    };
+
+    var leaderboarddef = {
+
+        // New fields
+        client: { type: ObjectId, ref: 'trn_clients' },
+        tournament: { type: ObjectId, ref: 'tournaments' },
+        tournament_match: { type: ObjectId, ref: 'trn_matches' },  // optional if bound to one specific match (not on a whole tournament)
+
+        title: { type: Schema.Types.Mixed },
+        info: { type: Schema.Types.Mixed },
+
+        active: { type: Boolean, default: true },
+
+        // The number of best scores per user. If null leaderboard is comprised by all user scores.  
+        bestscores: Number,
+
+        prizes: [{ type: prizeDef }],
+
+        // conditions: [{ condition: "Country", value:["GR","UK","SA"]}, {condition: "Age", value:["17"]}]
+        country: [{ type: String }],
+
+        created: { type: Date, default: Date.now }
+    };
+
+    var leaderboardDefSchema = new Schema(leaderboarddef);
+
+    module.exports = mongoose.model('trn_leaderboard_defs', leaderboardDefSchema);
+}
