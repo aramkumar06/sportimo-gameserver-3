97e998b6ce05a8f2f680ed7fbbe5b531fa4d9fc2
RabidRabbit <aris@bedbugstudio.com>
2019-12-03 14:45:25 +0200

    fixed achievement schemas and connections

--- sportimo_modules/models/achievement.js
+++ sportimo_modules/models/achievement.js
@@ -1,24 +1,21 @@
-// 'use strict';
+'use strict';
 
-// var mongoose = require('mongoose'),
-// Schema = mongoose.Schema,
-// ObjectId = Schema.ObjectId;
+var mongoose = require('mongoose'),
+Schema = mongoose.Schema,
+ObjectId = Schema.ObjectId;
 
-// var fields = {
-// 		uniqueid: { type: String }
-// 			,
-// 	icon: { type: String }
-// 			,
-// 	title: { type: String }
-// 			,
-// 	text: { type: String }
-// 			,
-// 	has: { type: Number },
-// 	total: { type: Number },
-// 	completed: { type: Boolean },
-// 	created: { type: Date , default: Date.now }
-// };
+var fields = {
+	uniqueid: String,
+    icon: String,
+    title: mongoose.Schema.Types.Mixed,
+    text: mongoose.Schema.Types.Mixed,
+    has: Number,
+    value: Number,
+    total: Number,
+    completed: Boolean,
+	created: { type: Date , default: Date.now }
+};
 
-// var achievementSchema = new Schema(fields);
+var achievementSchema = new Schema(fields);
 
-// module.exports = mongoose.model('achievements', achievementSchema);
+module.exports = mongoose.model('achievements', achievementSchema);
